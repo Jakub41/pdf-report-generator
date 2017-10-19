b4656945614d836f691a4283c3c2a298b9acbb47
Jakub41 <lemiszewski@gmx.com>
2017-10-19 11:05:47 +0200

    Inizial Commit

---
+++ lib/pdf/report/generator.rb
@@ -0,0 +1,9 @@
+require "pdf/report/generator/version"
+
+module Pdf
+  module Report
+    module Generator
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/pdf/report/generator/version.rb
@@ -0,0 +1,7 @@
+module Pdf
+  module Report
+    module Generator
+      VERSION = "0.1.0"
+    end
+  end
+end
